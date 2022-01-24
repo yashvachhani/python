84c8e9795d46a9ac03eb483b99bfe45544db2990
yash vachhani <yashvachhani88@gmail.com>
2022-01-24 21:45:25 +0000

    added some files

--- Q.py
+++ Q.py
@@ -12,21 +12,15 @@ arr[l[0]][l[1]]=l[2]
 for i in arr:
    print(i)
 
-
-
-
-
-for i in range((2*l[2])-1):
-   print(range(l[0]-i,l[0]+1,1))
-   print(i)
 f=1
 for i in range (l[0]-l[2],l[0],1):
    for j in range(l[1],f,1):
       arr[i][l[1]+f]=2
+      print(arr[i][l[1]+f])
+      print("j is "+ j )
    f= f+1
-   print(f)
-   #arr[i][10]=1  
-   #print(arr[i][10])
+   arr[i][10]=1 
+   print(arr[i][10])
 
 for i in arr:
    print(i)

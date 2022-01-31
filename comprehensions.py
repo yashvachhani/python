a87259ad549d09e6d5147619be46ee25ed5aae04
yash vachhani <yashvachhani88@gmail.com>
2022-01-31 23:29:54 +0000

    complet comprehensions

--- comprehensions.py
+++ comprehensions.py
@@ -46,3 +46,25 @@ print(my_list)
 
 my_list = [(letter,num) for letter in 'abcd' for num in range(4)]
 print(my_list)
+
+# how to use zip
+names = ['Bruce','Clark','peter','logan','wade']
+heros = ['Batman','Superman','spiderman','Wolverine','Deadpool']
+print(list(zip(names,heros)))
+my_dict = {}
+
+#dict comprihantion using zip
+my_dict = {names:heros for names,heros in zip(names,heros) if names != 'Clark'}
+print(my_dict)
+
+
+num = [1,2,3,4,5,6,7,8,9,0,9,8,7,5,4,3,2,5,6,6,6,6,7,7,5,3,2,2,3,4,4,5,5,6,6]
+# set comprihantion
+my_set = {n for n in nums}
+print(my_set)
+
+# genrator function
+my_gen = (n*n for n in nums)
+
+for i in my_gen:
+   print(i)

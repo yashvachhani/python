de617bd1e054742b861b718719c401d8dba547dc
yash vachhani <yashvachhani88@gmail.com>
2022-02-28 20:19:19 +0000

    complited list comprehension

--- list_comprehensions.py
+++ list_comprehensions.py
@@ -1,6 +1,5 @@
 num = [1,2,3,4,5,6,7,8,9,10]
 
-
 # n for each n in nums
 
 my_list=[]
@@ -41,10 +40,27 @@ print(my_list)
 
 my_list = filter(lambda n:n%2==0, num)
 
-print(list(my_list)
+#print(list(my_list))
 
-#I want a (latter, nuum) pair for each letter in 'abcd' and each number in '0123'
+# I want a (latter, nuum) pair for each letter in 'abcd' and each number in '0123'
+
+my_list=[]
 for letter in 'abcd':
    for num in range(4):
       my_list.append((letter,num))
 print(my_list)
+
+# 'abcd' and '0123' using comprihantion
+
+my_list = []
+my_list = [(latter,num) for latter in 'abcd' for num in range(4)]
+print(my_list)
+
+# dictionary comprihanion
+
+names = ['Bruce','Clark','Peter','Logan','Wade']
+heros = ['Batman','Deadpool','Wolerine','Spiderman','Superman']
+print(list(zip(names,heros)))
+
+my_dict = {names:heros for names,heros in zip(names,heros) if names != 'Peter' }
+print(my_dict)

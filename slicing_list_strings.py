fc115357d997c4e1431b48fa1b8e5bae561a32a6
yash vachhani <yashvachhani88@gmail.com>
2022-03-08 22:43:45 +0000

    added random things in slicing

--- slicing_list_strings.py
+++ slicing_list_strings.py
@@ -1,7 +1,8 @@
 my_list = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
 #          0  1  2  3  4  5  6  7  8  9
 #         -10-9 -8 -7 -6 -5 -4 -3 -2 -1
-
+#          indexes 
+ 
 #list[start:end:step]
 
 print(my_list[0:6])

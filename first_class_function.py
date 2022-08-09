3a424420fd7fc5d3d5482e0b7ce29db331260446
Yash Vachhani <yashvachhani88@gmail.com>
2022-08-09 23:22:34 +0100

    complet first class function

--- first_class_function.py
+++ first_class_function.py
@@ -28,3 +28,52 @@ print(f)
 
 f = square
 print(f)
+
+# use variable as a function
+print(f(5))
+
+# create map function for first class function understanding
+# calling function from another function
+def my_map(func,arg_list):
+	result = []
+	for i in arg_list:
+		result.append(func(i))
+	return result
+
+squares = my_map(square,[1,2,3,4,5])
+
+def cube(x):
+	return x * x * x
+
+cube = my_map(cube,[5,4,3,2,1])
+
+print(squares)
+print(cube)
+
+# create another log function for simpler massage
+# returnning function from another function
+def logger(msg):
+	def log_message():
+		print('Log: ', msg)
+	return log_message
+
+log_hi = logger('Hi!')
+log_hi()
+
+# function that returns function
+
+def html_tag(tag):
+	def wrap_text(msg):
+		print(f'{tag} {msg}')
+	return wrap_text
+
+print_hi = html_tag('hi')
+print_hi('Test headline!')
+print_hi('Another headline!')
+
+print_hi = html_tag('p')
+print_hi('Test Paragraph!')
+
+p = html_tag('yash')
+p('vachhani')
+

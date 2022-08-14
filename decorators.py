5cbeeaed91bc9f45b16ea9a773ad7460b09b6a84
Yash Vachhani <yashvachhani88@gmail.com>
2022-08-14 18:56:13 +0100

    worked on decoretors

--- decorators.py
+++ decorators.py
@@ -18,3 +18,37 @@ yash()
 my_func()
 my_func()
 
+#################################
+## outer functions and decorators
+#################################
+
+def decorator_function(message):
+	def wrapper_function():
+		print(message)
+	return wrapper_function
+
+hi_func = outer_function('Hi')
+bye_func = outer_function('Bye')
+
+hi_func()
+bye_func()
+
+#####################
+## simplest decorator
+#####################
+
+def decorator_function(original_function):
+	print(f'decorator function run start')
+	def wrapper_function():
+		print(f'weapper function start running')
+		print(f'{original_function.__name__} is yash')
+		return original_function()
+	print(f'decorator fuction complit')
+	return wrapper_function
+
+def display():
+	print('display function run start')
+	print('display function rjun complet')
+
+decorated_display = decorator_function(display)
+decorated_display()
